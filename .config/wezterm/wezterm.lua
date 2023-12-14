bf35b004dd0ee67873dd38b05c9ab180ec6e345b
Ahwx <ahwx@ahwx.org>
2023-12-14 16:08:10 +0100

    feat(wezterm/wezterm.lua): adds new wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,26 +7,26 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.window_decorations = "RESIZE"
+-- config.window_decorations = "RESIZE"
 config.color_scheme = 'Catppuccin Mocha'
 config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
 
 config.font = wezterm.font_with_fallback({
   {
-    family = 'ComicShannsMono Nerd Font Mono',
-    weight = "Bold"
+    family = 'ComicShannsMono Nerd Font',
+    weight = "Regular"
   },
   {
-    family = "ComicShannsMono Nerd Font Mono",
-    weight = "Bold"
+    family = "ComicShannsMono Nerd Font",
+    weight = "Regular"
   }
 })
 
 config.harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' }
 config.window_background_opacity = 0.8
 config.send_composed_key_when_left_alt_is_pressed = true
-config.font_size = 15.0
+config.font_size = 10.0
 config.cell_width = 0.88888888887 -- 8 / 9 rounded down
 config.front_end = "WebGpu"
 config.animation_fps = 1
