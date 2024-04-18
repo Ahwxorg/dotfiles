44aacc5035aef3f3c6272a6ec25c0908b04cd084
Ahwx <ahwx@ahwx.org>
2024-04-18 21:07:18 +0200

    feat: hyprland changes

--- .config/hypr/scripts/weather.sh
+++ .config/hypr/scripts/weather.sh
@@ -7,8 +7,8 @@ if [ ! -d $cachedir ]; then
     mkdir -p $cachedir
 fi
 
-if [ ! -f $cachedir/$cachefile ]; then
-    touch $cachedir/$cachefile
+if [ ! -f "$cachedir/$cachefile" ]; then
+    touch "$cachedir/$cachefile"
 fi
 
 # Save current IFS
@@ -17,24 +17,24 @@ SAVEIFS=$IFS
 IFS=$'\n'
 
 cacheage=$(($(date +%s) - $(stat -c '%Y' "$cachedir/$cachefile")))
-if [ $cacheage -gt 1740 ] || [ ! -s $cachedir/$cachefile ]; then
-    data=($(curl -s https://en.wttr.in/$1\?0qnT 2>&1))
-    echo ${data[0]} | cut -f1 -d, > $cachedir/$cachefile
-    echo ${data[1]} | sed -E 's/^.{15}//' >> $cachedir/$cachefile
-    echo ${data[2]} | sed -E 's/^.{15}//' >> $cachedir/$cachefile
+if [ $cacheage -gt 1740 ] || [ ! -s "$cachedir/$cachefile" ]; then
+    data=($(curl -s "https://en.wttr.in/$1\?0qnT 2>&1"))
+    echo "${data[0]}" | cut -f1 -d, > "$cachedir/$cachefile"
+    echo "${data[1]}" | sed -E 's/^.{15}//' >> "$cachedir/$cachefile"
+    echo "${data[2]}" | sed -E 's/^.{15}//' >> "$cachedir/$cachefile"
 fi
 
-weather=($(cat $cachedir/$cachefile))
+weather=($(cat "$cachedir/$cachefile"))
 
 # Restore IFSClear
 IFS=$SAVEIFS
 
-temperature=$(echo ${weather[2]} | sed -E 's/([[:digit:]])+\.\./\1 to /g')
+temperature=$(echo "${weather[2]}" | sed -E 's/([[:digit:]])+\.\./\1 to /g')
 
 #echo ${weather[1]##*,}
 
 # https://fontawesome.com/icons?s=solid&c=weather
-case $(echo ${weather[1]##*,} | tr '[:upper:]' '[:lower:]') in
+case $(echo "${weather[1]##*,}" | tr '[:upper:]' '[:lower:]') in
 "clear" | "sunny")
     condition=" "
     ;;
